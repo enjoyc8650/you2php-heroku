aa205ccfd5bd3a34c46e41fd2a513aab511bdccd
enjoyc8650 <50175262+enjoyc8650@users.noreply.github.com>
2019-05-02 23:44:04 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,6 +1,6 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY"));
+define('APIKEY', getenv("AIzaSyB8Jep2Dv2Xu_Mv6xsX-n386327Uyr3x8c"));
 define('GJ_CODE', getenv("GJ_CODE"));
 define('SITE_NAME',getenv("SITE_NAME"));
 define('TITLENAME', getenv("TITLENAME"));
